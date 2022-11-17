1cb718c48a661f2da5810c9ce66e27dc7ee60ad9
taeeon kim <myhom43@gmail.com>
2022-11-17 16:08:13 +0900

    layout, csr, ssg, isr

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+//모든페이지를 품을수 있는
+import Layout from "../components/Layout";
+
+export default function App({ Component, pageProps }) {
+  console.log(Component.getLayout);
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+  return getLayout(<Component {...pageProps} />);
+}
